9f81df146708573f611b78d70ee685aae6d1b244
juliano Ferreira <julianooen@hotmail.com>
2021-12-31 13:32:38 -0300

    juros compostos completo

--- compost interest/compost interest.js
+++ compost interest/compost interest.js
@@ -1,13 +1,86 @@
-
-
-
-
-
-function compostInterest(capital,taxa,tempo){
+/**
+ * calcula o montante em juros compostos
+ * @param {double} capital 
+ * @param {double} taxa 
+ * @param {double} tempo 
+ * @returns montante (double)
+ */
+function compostInterestMontante(capital,taxa,tempo){
     let montante = 0;
-    montante = parseFloat(capital.value)*(Math.pow(1+parseFloat(taxa.value)/100,parseFloat(tempo.value)))
-    return alert(`Juros Compostos:\n O valor dos juros é: ${montante - capital.value}\n O montante final será de ${montante}`)
+
+    montante = parseFloat(capital) * (Math.pow(1 + parseFloat(taxa) / 100 , parseFloat(tempo)));
+
+    return montante;
     
     }
     
-    
+
+
+/**
+ * calcula o o valor dos juros total em juros compostos
+ * @param {double} capital 
+ * @param {double} taxa 
+ * @param {double} tempo 
+ * @returns juros (double)
+ */
+function compostInterestJuros(capital,taxa,tempo){
+
+    let juros = 0;
+
+    juros = compostInterestMontante(capital,taxa,tempo) - capital;
+
+    return juros;
+}
+
+
+/**
+ * calcula taxa em juros compostos
+ * @param {double} capital 
+ * @param {double} montante 
+ * @param {double} tempo 
+ * @returns taxa (double)
+ */
+function compostInterestTaxa(capital,montante,tempo){
+    let taxa = 0;
+
+    taxa = 100 * ((Math.pow(montante / capital,(1 / tempo))) - 1);
+
+    return taxa;
+}
+
+
+/**
+ * calcula o capital inicial em juros compostos
+ * @param {double} montante 
+ * @param {double} taxa 
+ * @param {double} tempo 
+ * @returns capital (double)
+ */
+function compostInterestCapital(montante,taxa,tempo){
+    let capital = 0;
+
+    capital = montante / Math.pow(((taxa / 100) + 1),tempo);
+
+    return capital;
+}
+
+
+/**
+ * calcula o tempo em juros compostos
+ * @param {double} capital 
+ * @param {double} taxa 
+ * @param {double} tempo 
+ * @returns tempo (double)
+ */
+function compostInterestTempo(capital,montante,taxa){
+    let log1 = Math.log10(montante / capital);
+
+    let log2 = Math.log10((taxa / 100) + 1);
+
+    let tempo = log1 / log2;
+
+    return tempo
+
+}
+
+
